8916d9fd2b41c1958b0615759b5510754d4dccf0
Adrien Pyke <adpyke@gmail.com>
2019-09-24 18:57:25 -0400

    fix stripInitialIndent

--- webview/index.js
+++ webview/index.js
@@ -6,9 +6,12 @@ let fontFamily;
 let enableLigatures;
 
 const stripInitialIndent = node => {
+	console.log(node.innerHTML);
 	const initialSpans = Array.from(node.querySelectorAll('div > span:first-child'));
-	if (initialSpans.some(span => !span.textContent.match(/^\s+$/))) return;
-	const minIndent = Math.min(...initialSpans.map(span => span.textContent.length));
+	if (initialSpans.some(span => !span.textContent.match(/^\s+/))) return;
+	const minIndent = Math.min(
+		...initialSpans.map(span => span.textContent.match(/^\s+/)[0].length)
+	);
 	initialSpans.forEach(span => (span.textContent = span.textContent.slice(minIndent)));
 };
 
