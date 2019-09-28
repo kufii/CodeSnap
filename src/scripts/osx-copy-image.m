73bd35433711922a2e90fc9a5966b2eb9f8cb57f
Adrien Pyke <adpyke@gmail.com>
2019-09-28 10:03:14 -0400

    objective c formatting

--- src/scripts/osx-copy-image.m
+++ src/scripts/osx-copy-image.m
@@ -2,17 +2,18 @@
 #import <Foundation/Foundation.h>
 #import <Cocoa/Cocoa.h>
 #import <unistd.h>
+
 BOOL copy_to_clipboard(NSString *path)
 {
-  NSImage * image;
-  if([path isEqualToString:@"-"])
+  NSImage *image;
+  if ([path isEqualToString:@"-"])
   {
     NSFileHandle *input = [NSFileHandle fileHandleWithStandardInput];
     image = [[NSImage alloc] initWithData:[input readDataToEndOfFile]];
   }
   else
   {
-    image =  [[NSImage alloc] initWithContentsOfFile:path];
+    image = [[NSImage alloc] initWithContentsOfFile:path];
   }
   BOOL copied = false;
   if (image != nil)
@@ -27,18 +28,18 @@ BOOL copy_to_clipboard(NSString *path)
   return copied;
 }
 
-int main(int argc, char * const argv[])
+int main(int argc, const char *argv[])
 {
   NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-  if(argc<2)
+  if (argc < 2)
   {
     printf("Usage:\n\n"
       "Copy file to clipboard:\n    ./osx-copy-image path/to/file\n\n"
       "Copy stdin to clipboard:\n    cat /path/to/file | ./osx-copy-image -");
     return EXIT_FAILURE;
   }
-  NSString *path= [NSString stringWithUTF8String:argv[1]];
+  NSString *path = [NSString stringWithUTF8String:argv[1]];
   BOOL success = copy_to_clipboard(path);
   [pool release];
-  return (success?EXIT_SUCCESS:EXIT_FAILURE);
+  return (success ? EXIT_SUCCESS : EXIT_FAILURE);
 }
